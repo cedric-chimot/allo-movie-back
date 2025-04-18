96b2e65ac3fae009cb725198e80f9f270d2791b3
Cedric Chimot <cedric_om10@yahoo.fr>
2025-04-18 14:40:45 +0200

    first commit

---
+++ src/test/java/fr/cch/allo_movie/AlloMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cch.allo_movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlloMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
