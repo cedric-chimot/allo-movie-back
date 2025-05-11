5330a07e0f1493a7c9a802e0dcc3ca7a64bebb61
Cedric Chimot <cedric_om10@yahoo.fr>
2025-05-11 10:00:19 +0200

    ajout d'une énum pour les motifs de signalements

--- src/main/java/fr/cch/allo_movie/entity/Signalements.java
+++ src/main/java/fr/cch/allo_movie/entity/Signalements.java
@@ -1,5 +1,6 @@
 package fr.cch.allo_movie.entity;
 
+import fr.cch.allo_movie.enums.MotifsEnum;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -19,8 +20,9 @@ public class Signalements {
   @Column(name = "id")
   private Long id;
 
+  @Enumerated(EnumType.STRING)
   @Column(name = "motif")
-  private String motif;
+  private MotifsEnum motif;
 
   @Column(name = "date_signalement")
   private Long dateSignalement;
@@ -36,7 +38,7 @@ public class Signalements {
   @JoinColumn(name = "id_reponse")
   private Reponses reponseSignal;
 
-  public Signalements(String motif, Long dateSignalement, String message, Users userSignal, Reponses reponseSignal) {
+  public Signalements(MotifsEnum motif, Long dateSignalement, String message, Users userSignal, Reponses reponseSignal) {
     this.motif = motif;
     this.dateSignalement = dateSignalement;
     this.message = message;
---
+++ src/main/java/fr/cch/allo_movie/enums/MotifsEnum.java
@@ -0,0 +1,14 @@
+package fr.cch.allo_movie.enums;
+
+public enum MotifsEnum {
+  INSULTES,                    // langage offensant, insultes
+  SPAM_OU_PUBLICITE,           // lien suspect, pub déguisée
+  HORS_SUJET,                  // n'a rien à voir avec la discussion
+  HARCELEMENT_OU_INTIMIDATION, // attaque ou menace envers quelqu’un
+  CONTENU_VULGAIRE,            // contenu choquant ou explicite
+  INFORMATION_PERSONNELLE,     // données privées exposées
+  USURPATION_IDENTITE,         // se fait passer pour un autre
+  INCITATION_A_LA_HAINE,       // propos discriminants ou violents
+  VIOLATION_DROITS_AUTEUR,     // contenu copié sans autorisation
+  AUTRES                       // autres motifs à justifier
+}
