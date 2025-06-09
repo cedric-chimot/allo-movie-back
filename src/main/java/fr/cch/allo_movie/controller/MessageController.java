d3684046444502aef09cda865de20fb363f71189
Cedric Chimot <cedric_om10@yahoo.fr>
2025-06-09 14:38:03 +0200

    paramétrage service et controller message

--- src/main/java/fr/cch/allo_movie/controller/MessageController.java
+++ src/main/java/fr/cch/allo_movie/controller/MessageController.java
@@ -1,4 +1,90 @@
 package fr.cch.allo_movie.controller;
 
+import fr.cch.allo_movie.entity.Message;
+import fr.cch.allo_movie.entity.Users;
+import fr.cch.allo_movie.service.MessageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/messages")
+@CrossOrigin
 public class MessageController {
+
+  /**
+   * Le service des messages
+   */
+  private final MessageService messageService;
+
+  /**
+   * Le constructeur
+   * @param messageService injection du service message
+   */
+  public MessageController(MessageService messageService) {
+    this.messageService = messageService;
+  }
+
+  /**
+   * Créer un nouveau message
+   * @param content le contenu du message
+   * @param userId l'id de l'utilisateur
+   * @return le nouveau message
+   */
+  @PostMapping("/create")
+  public ResponseEntity<Message> createMessage(@RequestParam String content, @RequestParam Long userId) {
+    Message message = messageService.saveMessage(content, userId);
+    return ResponseEntity.ok(message);
+  }
+
+  /**
+   * Afficher la liste de tous les messages
+   * @return la liste des messages
+   */
+  @GetMapping("/all")
+  public ResponseEntity<List<Message>> getAllMessages() {
+    return ResponseEntity.ok(messageService.getAllMessages());
+  }
+
+  /**
+   * Trouver un message par son id
+   * @param id l'id d'un message
+   * @return le message recherché
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<Message> getMessageById(@PathVariable Long id) {
+    return ResponseEntity.ok(messageService.getMessageById(id));
+  }
+
+  /**
+   * Mettre à jour un message
+   * @param id l'id du message à mettre à jour
+   * @param content le contenu à mettre à jour
+   * @return le message mis à jour
+   */
+  @PatchMapping("/update/{id}")
+  public ResponseEntity<Message> updateMessage(@PathVariable Long id, @RequestParam String content) {
+    Message updatedMessage = messageService.updateMessage(id, content);
+    return ResponseEntity.ok(updatedMessage);
+  }
+
+  /**
+   * Supprimer un message par son id
+   * @param id l'id de le message à supprimer
+   * @return le message supprimé
+   */
+  @DeleteMapping("/delete/{id}")
+  public Message deleteMessageById(@PathVariable Long id) {
+    return messageService.deleteById(id);
+  }
+
+  /**
+   * Supprimer tous les messages
+   */
+  @DeleteMapping("/delete/all")
+  public void deleteAllMessages() {
+    messageService.deleteAll();
+  }
+
 }
--- src/main/java/fr/cch/allo_movie/service/MessageService.java
+++ src/main/java/fr/cch/allo_movie/service/MessageService.java
@@ -1,4 +1,106 @@
 package fr.cch.allo_movie.service;
 
+import fr.cch.allo_movie.entity.Message;
+import fr.cch.allo_movie.entity.Users;
+import fr.cch.allo_movie.exceptions.CustomException;
+import fr.cch.allo_movie.repository.MessageRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MessageService {
+
+  private final MessageRepository messageRepository;
+  private final UsersService usersService;
+
+  /**
+   * Le constructeur du service
+   * @param messageRepository injection du répo message
+   * @param usersService injection du répo users
+   */
+  public MessageService(MessageRepository messageRepository, UsersService usersService) {
+    this.messageRepository = messageRepository;
+    this.usersService = usersService;
+  }
+
+  /**
+   * Ajouter un nouveau message
+   * @param content le contenu du message
+   * @param userId l'id de l'utilisateur envoyant le message
+   * @return le nouveau message
+   */
+  public Message saveMessage(String content, Long userId) {
+    Users user = usersService.findById(userId);
+
+    Message message = new Message();
+    message.setMessage(content);
+    message.setUserMessage(user);
+    message.setDateMessage(LocalDateTime.now());
+
+    return messageRepository.save(message);
+  }
+
+  /**
+   * Méthode pour trouver tous les messages
+   * @return la liste des messages
+   */
+  public List<Message> getAllMessages() {
+    return messageRepository.findAll();
+  }
+
+  /**
+   * Récupérer un message par ID
+   * @param id l'id du message recherché
+   * @return L'objet trouvé
+   */
+  public Message getMessageById(Long id) {
+    return messageRepository.findById(id)
+      .orElseThrow(() -> new RuntimeException("Message non trouvé"));
+  }
+
+  /**
+   * Mettre à jour un message
+   * @param id l'id du message
+   * @param content le contenu du message
+   * @return le message mis à jour
+   */
+  public Message updateMessage(Long id, String content) {
+    Message existingMessage = messageRepository.findById(id)
+      .orElseThrow(() -> new RuntimeException("Message non trouvé"));
+
+    existingMessage.setMessage(content);
+    existingMessage.setDateMessage(LocalDateTime.now());
+
+    return messageRepository.save(existingMessage);
+  }
+
+  /**
+   * Supprimer un message par son id
+   * @param id l'id du message à supprimer
+   * @return L'objet supprimé
+   */
+  public Message deleteById(Long id) {
+    // Récupérer l'objet dans un Optional
+    Optional<Message> optionalUsers = messageRepository.findById(id);
+
+    // Vérifier si l'objet existe
+    if (optionalUsers.isPresent()) {
+      Message message = optionalUsers.get();
+      messageRepository.delete(message); // Supprimer l'objet
+      return message; // Retourner l'objet supprimé
+    } else {
+      throw new CustomException("Message", "id",  id);
+    }
+  }
+
+  /**
+   * Supprimer tous les messages
+   */
+  public void deleteAll() {
+    messageRepository.deleteAll();
+  }
+
 }
